8b9f82b68d94084b4a46c9f691595deeb7e87abd
JoaoPerpetua <joaoperpetua@ua.pt>
2021-03-20 10:12:02 +0000

    example

---
+++ guideFiles/P3/test_server_TCP.c
@@ -0,0 +1,87 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT     56789 
+#define BUFFERSIZE 1024 
+   
+int main( int argc, char *argv[] ) {
+    int sockfd, newsockfd, clilen, n;
+    char buffer[BUFFERSIZE];
+    struct sockaddr_in servaddr;
+    char *hello = "Hello from server"; 
+    int HostID = 1;
+    struct cliaddr[1000];
+    // Creating a TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+   
+    /* Initialize socket structure */
+    bzero(&servaddr, sizeof(servaddr)); 
+    bzero(&cliaddr, sizeof(cliaddr)); 
+   
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+   
+    /* Bind the socket with the server address - This links the 
+    *socket to the actual physical address and port number of the 
+    *server, in such way that from now on we only need to listen 
+    *to the port to receive any incoming server requests.
+    */
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) 
+    {
+        perror("ERROR on binding");
+        exit(1);
+    }
+      
+    /* Now start listening for the clients, here process will
+     * go in sleep mode and will wait for the incoming connection
+    */
+    while(1){
+    	listen(sockfd,5);
+    	clilen = sizeof(cliaddr[HostID]);
+   
+    	// Accept actual connection from the client 
+    	newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
+	
+	
+
+    	if (newsockfd < 0) 
+    	{
+        	perror("ERROR on accept");
+        	exit(1);
+    	}
+    	char *welcome = "Welcome to the server, your Host ID is ="+HostID;
+    	char *totalclient = "Nº of total clients = "+nclients;
+    	// Send a response to the client 
+    	n = send(newsockfd, welcome, strlen(welcome), 0);
+    	n = send(newsockfd, totalclient, strlen(totalclient),0);
+    	if (n < 0) {
+        	perror("ERROR writing to socket");
+        	exit(1);
+    	}
+    	
+    	// If connection is established then start communicating 
+    	n = recv(newsockfd, buffer, BUFFERSIZE, 0);
+    	if (n < 0) 
+    	{
+        	perror("ERROR reading from socket");
+        	exit(1);
+    	}
+    	buffer[n] = '\0';
+    	printf("Client : %s\n", buffer); 
+   
+    	
+    	
+        HostID++;
+    }
+    return 0;
+}
